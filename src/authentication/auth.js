434e7a8a445de4c3dd3c23647f1a825bc9c046db
Teheskhiel Fritz <teheskhielfritz@gmail.com>
2024-06-20 07:26:40 +0700

    auth update

--- src/authentication/auth.js
+++ src/authentication/auth.js
@@ -1,43 +1,35 @@
+// middleware/auth.middleware.js
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-const authenticateUser = async(req, res, next) => {
+const authenticateUser = async (req, res, next) => {
     try {
-
-        const token = req.headers.authorization?.split(' ')[1]
+        const token = req.headers.authorization?.split(' ')[1];
         if (!token) {
-            res.status(401)
-                .json({ message: 'Unauthorized'})
+            return res.status(401).json({ message: 'Unauthorized' });
         }
 
-        const decoded = jwt.verify(token, 'secret')
-
-        const user = await User.findById(decoded.user)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.user_id);
         if (!user) {
-            res.status(401)
-                .json({ message: 'Unauthorized'})
+            return res.status(401).json({ message: 'Unauthorized' });
         }
 
-        req.user = user
-        next()
-
+        req.user = user;
+        next();
     } catch (err) {
-      if (err.username === 'TokenExpiredError') {
-        res.status(401)
-          .json({message: 'Token has expired' })
-      }
-        res.status(500)
-            .json({ message: 'Invalid token'})
+        if (err.name === 'TokenExpiredError') {
+            return res.status(401).json({ message: 'Token has expired' });
+        }
+        res.status(500).json({ message: 'Invalid token' });
     }
-}
+};
 
-const isAdmin = async(req, res) => {
-
-    const adminRole = req.user.role
-    if (adminRole !== 'admin') {
-        res.status(401)
-            .json({ message: 'Forbidden you are not admin'})
+const isAdmin = (req, res, next) => {
+    if (req.user.role !== 'admin') {
+        return res.status(403).json({ message: 'Forbidden: You are not an admin' });
     }
-}
+    next();
+};
 
-module.exports = { authenticateUser, isAdmin }
+module.exports = { authenticateUser, isAdmin };
